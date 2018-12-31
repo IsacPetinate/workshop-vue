0720a740d667d891b717a3f626f73b6f1d902eef
isacpetinate <isac.petinate@fatec.sp.gov.br>
2018-12-31 03:06:34 -0200

    Criando Componentes

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app>
     <v-toolbar dark app>
-      <v-toolbar-title class="headline text-uppercase">
-        
-        <span class="font-weight-light">CheckList</span>
+      <v-toolbar-title class="headline">
+        <v-icon>format_list_numbered</v-icon>
+        <span class="font-weight-light"> Minha Lista</span>
       </v-toolbar-title>
     </v-toolbar>
-
+    <!--  -->
     <v-content>
       <router-view/>
     </v-content>
